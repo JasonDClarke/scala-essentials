9939bf2e0f87e97d5da4936c2de03893a284e366
Jason Clarke <jasondanielclarke@gmail.com>
2025-05-09 08:09:45 +0100

    Abstract data types part 1

---
+++ src/main/scala/lectures/part2oop/AbstractDataTypes.scala
@@ -0,0 +1,36 @@
+package lectures.part2oop
+
+object AbstractDataTypes extends App {
+
+  // abstract class with abstract members
+  abstract class Animal {
+    val creatureType: String
+    def eat: Unit
+  }
+  // abstract classes cannot be instantiated
+//  val animal = new Animal // Class Animal is abstract, cannot be instantiated
+
+  class Dog extends Animal {
+    override val creatureType: String = "Canine"
+    override def eat: Unit = println("crunch crunch")
+  }
+  //  override keyword is NOT required for overriding fields from abstract classes
+
+  // traits
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  trait ColdBlooded
+
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    val creatureType: String = "croc"
+    def eat = "nomnomnom"
+    def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}")
+  }
+
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
+
+}
