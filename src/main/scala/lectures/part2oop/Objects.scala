4298ec1f2b3eaa0d00f4ec21591f7805715d1f6a
Jason Clarke <jasondanielclarke@gmail.com>
2025-05-09 06:41:23 +0100

    Objects and main method

--- src/main/scala/lectures/part2oop/Objects.scala
+++ src/main/scala/lectures/part2oop/Objects.scala
@@ -1,26 +1,37 @@
 package lectures.part2oop
 
-object Objects extends App {
+object Objects {
   // SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY ("static")
   object Person { // type + its only instance
     // "static"/"class" - level functionality
     val N_EYES = 2
     def canFly: Boolean = false
+
+    // factory method
+    def apply(mother: Person, father: Person): Person = new Person("Bobbie")
+
   }
-  class Person {
+  class Person(val name: String = "Bob") {
     // instance-level functionality
   }
   // COMPANIONS
+  def main(args: Array[String]): Unit =
+  {
+    println(Person.N_EYES)
+    println(Person.canFly)
 
-  println(Person.N_EYES)
-  println(Person.canFly)
+    // Scala object = SINGLETON INSTANCE
+    val mary = Person;
+    val john = Person;
+    println(mary == john) // true
 
-  // Scala object = SINGLETON INSTANCE
-  val mary = Person;
-  val john = Person;
-  println(mary == john) // true
+    val maryInstance = new Person;
+    val johnInstance = new Person;
+    println(maryInstance == johnInstance) // false
 
-  val maryInstance = new Person;
-  val johnInstance = new Person;
-  println(maryInstance == johnInstance) // false
+    val bobbie = Person(maryInstance, johnInstance) // Person.apply(maryInstance, johnInstance)
+
+    // Scala Applications = Scala object with
+    // def main(args: Array[string]): Unit
+  }
 }
